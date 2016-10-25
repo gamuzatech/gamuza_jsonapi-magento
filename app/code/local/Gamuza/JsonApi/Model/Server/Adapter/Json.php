eba5e25b5760adf840c43aa7ee985c34b8dfcf44
Eneias Ramos de Melo <eneias@gamuza.com.br>
2016-10-25 16:39:11 -0200

    Adicionado modulo Gamuza_JsonApi

---
+++ app/code/local/Gamuza/JsonApi/Model/Server/Adapter/Json.php
@@ -0,0 +1,114 @@
+<?php
+/*
+ * Gamuza JSON API - Fast API for magento platform.
+ * Copyright (c) 2016 Gamuza Technologies (http://www.gamuza.com.br/)
+ * Author: Eneias Ramos de Melo <eneias@gamuza.com.br>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+/*
+ * See the AUTHORS file for a list of people on the Gamuza Team.
+ * See the ChangeLog files for a list of changes.
+ * These files are distributed with gamuza_jsonapi-magento at http://github.com/gamuzabrasil/.
+ */
+
+class Gamuza_JsonApi_Model_Server_Adapter_Json
+extends Varien_Object
+implements Mage_Api_Model_Server_Adapter_Interface
+{
+
+protected $_json = null;
+
+public function setHandler($handler)
+{
+    $this->setData('handler', $handler);
+
+    return $this;
+}
+
+public function getHandler()
+{
+    return $this->getData('handler');
+}
+
+public function setController(Mage_Api_Controller_Action $controller)
+{
+     $this->setData('controller', $controller);
+
+     return $this;
+}
+
+public function getController()
+{
+    $controller = $this->getData('controller');
+
+    if (null === $controller)
+    {
+        $controller = new Varien_Object(
+            array('request' => Mage::app()->getRequest(), 'response' => Mage::app()->getResponse())
+
+        );
+        $this->setData('controller', $controller);
+    }
+
+    return $controller;
+}
+
+public function run()
+{
+    $apiConfigCharset = Mage::getStoreConfig("api/config/charset");
+
+    $this->_json = new Zend_Json_Server();
+    $this->_json // ->setEncoding($apiConfigCharset)
+        ->setClass($this->getHandler())
+        ->setAutoEmitResponse(false)
+        ->getRequest()->setId($this->getHelper()->getUniqId());
+    $this->getController()->getResponse()
+        ->clearHeaders()
+        ->setHeader('Content-Type','application/json; charset=' . $apiConfigCharset)
+        ->setBody($this->_json->handle());
+
+    return $this;
+}
+
+public function map()
+{
+    $apiConfigCharset = Mage::getStoreConfig("api/config/charset");
+
+    $this->_json = new Zend_Json_Server();
+    $this->_json // ->setEncoding($apiConfigCharset)
+        ->setClass($this->getHandler());
+    $this->getController()->getResponse()
+        ->clearHeaders()
+        ->setHeader('Content-Type', 'application/json; charset=' . $apiConfigCharset)
+        ->setBody($this->_json->getServiceMap());
+
+    return $this;
+}
+
+public function fault($code, $message)
+{
+    throw new Zend_Json_Server_Exception ($message, $code);
+}
+
+public function getHelper()
+{
+    return Mage::helper('jsonapi');
+}
+
+}
+
---
+++ app/code/local/Gamuza/JsonApi/Model/Server/Handler.php
@@ -0,0 +1,102 @@
+<?php
+/*
+ * Gamuza JSON API - Fast API for magento platform.
+ * Copyright (c) 2016 Gamuza Technologies (http://www.gamuza.com.br/)
+ * Author: Eneias Ramos de Melo <eneias@gamuza.com.br>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+/*
+ * See the AUTHORS file for a list of people on the Gamuza Team.
+ * See the ChangeLog files for a list of changes.
+ * These files are distributed with gamuza_jsonapi-magento at http://github.com/gamuzabrasil/.
+ */
+
+class Gamuza_JsonApi_Model_Server_Handler
+extends Mage_Api_Model_Server_Handler
+// extends Mage_Api_Model_Server_Handler_Abstract
+{
+
+protected function _getConfig()
+{
+    return Mage::getSingleton('jsonapi/config');
+}
+
+protected function _getServer()
+{
+    return Mage::getSingleton('jsonapi/server');
+}
+
+public function call($sessionId, $apiPath, $args = array())
+{
+    $this->_startSession($sessionId);
+
+    if (!$this->_getSession()->isLoggedIn($sessionId))
+    {
+        return $this->_fault('session_expired');
+    }
+
+    $cached = Mage::getStoreConfig('api/config/json_cache_enabled');
+    if ($cached)
+    {
+        $result = $this->_getConfig()->loadCacheResult ($this->_getCacheId($apiPath));
+
+        if (!empty ($result)) return unserialize ($result);
+    }
+
+    $result = parent::call ($sessionId, $apiPath, $args);
+    if ($cached)
+    {
+        $this->_getConfig()->saveCacheResult (serialize ($result), $this->_getCacheId($apiPath));
+    }
+
+    return $result;
+}
+
+public function multiCall($sessionId, array $calls = array(), $options = array())
+{
+    /*
+    $this->_startSession($sessionId);
+
+    if (!$this->_getSession()->isLoggedIn($sessionId))
+    {
+        return $this->_fault('session_expired');
+    }
+    */
+
+    $result = array();
+
+    foreach ($calls as $_call)
+    {
+        $apiPath = $_call [0];
+        $args    =  !empty ($_call [1]) ? $_call [1] : array ();
+
+        $result [] = $this->call ($sessionId, $apiPath, $args);
+    }
+
+    return $result;
+}
+
+protected function _getCacheId($apiPath)
+{
+    $id = $this->_getServer()->getApiName() . '.' . $apiPath;
+
+    return $id;
+}
+
+}
+
